23c9ec9f3002fe9e53730b09d040dcfd1204b808
jess12-sdk <jessica123lima987@gmail.com>
2020-08-12 16:41:48 -0300

    Primeiro commit

---
+++ Assets/Scripts/Box.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Box : MonoBehaviour
+{
+    public float jumpForce;
+    public bool isUp;
+
+    public Animator anim;
+
+    void Start()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            if(isUp)
+            {
+                anim.SetTrigger("hit");
+                collision.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+            }
+            else
+            {
+                anim.SetTrigger("hit");
+                collision.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, -jumpForce), ForceMode2D.Impulse);
+            }
+        }
+    }
+}
